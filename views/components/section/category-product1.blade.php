29831bca80867820a36fa50ed8efb5b7fd950876
SevenJobs <nguyenbabay96@gmail.com>
2021-09-17 04:44:46 +0700

    toi uu diem toc do google

--- views/components/section/category-product1.blade.php
+++ views/components/section/category-product1.blade.php
@@ -2,32 +2,55 @@
 
 <section class="section-custom sec-category-product bg-white lg:bg-auto antialiased">
     <div class="container-custom">
-        <div class="{{ 'swiperCategoryProduct'.$keySlide }} swiper-main swiper-container mySwiperGecko my-swiper-main overflow-hidden">
-            <div class="swiper-wrapper">
-                @foreach(get_categories_feature() as $category)
-                    <div class="swiper-slide">
-                        <div class="text-center rounded-t-full hover:shadow-xl">
-                            <a href="{!! $category->url !!}"
-                               class="inline-block w-full">
-                                <div class="relative" style="padding-bottom: calc( 1 * 100% )">
-                                    <img
-                                        data-src="{{ TnMedia::getImageUrl($category->image, 'thumb', asset('/images/no-image.jpg')) }}"
-                                        src="{{ asset('/images/no-image.jpg') }}"
-                                        class="w-full h-full block m-auto rounded-full lg:p-3 absolute lazyload"
-                                        alt="{{ $category->name }}"
-                                        style="background-image: linear-gradient(180deg, #ffffff, #f8f8f800);"
-                                    >
-                                </div>
-
-                                <div class="text-gray-600 font-bold line-clamp-1 text-xs md:text-base">
-                                    {{ $category->name }}
-                                </div>
-                            </a>
+        <div class="w-full grid grid-cols-6 lg:grid-cols-9 gap-2 lg:gap-4">
+            @foreach(get_categories_feature() as $category)
+                <div class="text-center rounded-t-full hover:shadow-xl">
+                    <a href="{!! $category->url !!}"
+                       class="inline-block w-full">
+                        <div class="relative" style="padding-bottom: calc( 1 * 100% )">
+                            <img
+                                data-src="{{ TnMedia::getImageUrl($category->image, 'thumb', asset('/images/no-image.jpg')) }}"
+                                src="{{ asset('/images/no-image.jpg') }}"
+                                class="w-full h-full block m-auto rounded-full lg:p-3 absolute lazyload"
+                                alt="{{ $category->name }}"
+                                style="background-image: linear-gradient(180deg, #ffffff, #f8f8f800);"
+                            >
                         </div>
-                    </div>
-                @endforeach
-            </div>
+
+                        <div class="text-gray-600 font-bold line-clamp-1 text-xs md:text-base">
+                            {{ $category->name }}
+                        </div>
+                    </a>
+                </div>
+            @endforeach
         </div>
+
+{{--        <div class="{{ 'swiperCategoryProduct'.$keySlide }} swiper-main swiper-container mySwiperGecko my-swiper-main overflow-hidden">--}}
+{{--            <div class="swiper-wrapper">--}}
+{{--                @foreach(get_categories_feature() as $category)--}}
+{{--                    <div class="swiper-slide">--}}
+{{--                        <div class="text-center rounded-t-full hover:shadow-xl">--}}
+{{--                            <a href="{!! $category->url !!}"--}}
+{{--                               class="inline-block w-full">--}}
+{{--                                <div class="relative" style="padding-bottom: calc( 1 * 100% )">--}}
+{{--                                    <img--}}
+{{--                                        data-src="{{ TnMedia::getImageUrl($category->image, 'thumb', asset('/images/no-image.jpg')) }}"--}}
+{{--                                        src="{{ asset('/images/no-image.jpg') }}"--}}
+{{--                                        class="w-full h-full block m-auto rounded-full lg:p-3 absolute lazyload"--}}
+{{--                                        alt="{{ $category->name }}"--}}
+{{--                                        style="background-image: linear-gradient(180deg, #ffffff, #f8f8f800);"--}}
+{{--                                    >--}}
+{{--                                </div>--}}
+
+{{--                                <div class="text-gray-600 font-bold line-clamp-1 text-xs md:text-base">--}}
+{{--                                    {{ $category->name }}--}}
+{{--                                </div>--}}
+{{--                            </a>--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                @endforeach--}}
+{{--            </div>--}}
+{{--        </div>--}}
     </div>
     <script>
         var categoryProduct{{$keySlide}} = new Swiper(".swiperCategoryProduct{{$keySlide}}", {
