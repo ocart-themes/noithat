16e03b2b7976cc598f769cd42819ca48d60b8e90
SevenJobs <nguyenbabay96@gmail.com>
2021-09-17 06:03:26 +0700

    toi uu diem toc do google

--- views/layouts/header.blade.php
+++ views/layouts/header.blade.php
@@ -165,12 +165,8 @@
                                 <div class="line-clamp-1" style="max-width: 120px">{{ Auth::user()->name }}</div>
 
                                 <div class="ml-1">
-                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
-                                         viewBox="0 0 20 20">
-                                        <path fill-rule="evenodd"
-                                              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                              clip-rule="evenodd"/>
-                                    </svg>
+                                    <x-theme::icons.chevron-down class="text-gray-400 h-4 w-4 group-hover:text-gray-500"/>
+
                                 </div>
                             </button>
                         </x-slot>
@@ -319,13 +315,8 @@
                                                                     x-on:click="openSubNav = ! openSubNav"
                                                                     class="cursor-pointer w-16 md:w-20 h-8 text-lg flex-shrink-0 flex items-center justify-center -mr-4"
                                                                 >
-                                                                    <svg class="text-gray-400 ml-2 h-5 w-5 group-hover:text-gray-500"
-                                                                         xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor"
-                                                                         aria-hidden="true">
-                                                                        <path fill-rule="evenodd"
-                                                                              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                                              clip-rule="evenodd"/>
-                                                                    </svg>
+                                                                    <x-theme::icons.chevron-down class="text-gray-400 ml-2 h-5 w-5 group-hover:text-gray-500"/>
+
                                                                 </div>
                                                             </div>
                                                         </div>
