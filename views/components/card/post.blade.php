39c1a36ee2c3a3e76e4a502b53879814cba36cde
SevenJobs <nguyenbabay96@gmail.com>
2021-09-17 22:36:00 +0700

    toi uu diem toc do google

--- views/components/card/post.blade.php
+++ views/components/card/post.blade.php
@@ -16,6 +16,7 @@
                     <img
                         class="w-full h-full object-cover {{ $rounded }} absolute lozad"
                         data-src="{{ TnMedia::getImageUrl($data->image, $sizeImage, asset('/images/no-image.jpg')) }}"
+                        data-srcset="{{ TnMedia::getImageUrl($data->image . '?w=200', asset('/images/no-image.jpg')) }} 1000w, {{ TnMedia::getImageUrl($data->image, $sizeImage, asset('/images/no-image.jpg')) }} 2000w"
                         src="{{ asset('/images/no-image.jpg') }}"
                         alt="{{ $data->name }}"
                     >
